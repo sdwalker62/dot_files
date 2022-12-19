395e828d5470675403460c307af8c44dfd514d52
Dalton Walker <dev@daltonwalker.com>
2022-12-19 01:09:41 -0500

    adds basic nvim configs

---
+++ nvim/init.lua
@@ -0,0 +1,26 @@
+require('plugins')
+
+
+require('lualine').setup {
+    options = {
+        theme = 'auto'
+    }
+}
+require('material').setup({
+    lualine_style = 'default'
+})
+vim.g.material_style = "deep ocean"
+vim.cmd 'colorscheme material'
+
+
+require'nvim-treesitter.configs'.setup {
+    highlight = {
+        enable = true, 
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
+}
+
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,16 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+    
+    -- themes
+    use 'marko-cerovac/material.nvim'
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+end)
